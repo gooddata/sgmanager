0ca578f950036e9ebf121719a5c492e5638954cd
Filip Pytloun <filip.pytloun@gooddata.com>
2013-04-19 12:07:01 +0200

    Fix dumping of groups and rules after refactoring, remove junk

--- gdc/sgmanager/__init__.py
+++ gdc/sgmanager/__init__.py
@@ -6,6 +6,7 @@ import os
 import boto
 import yaml
 from gdc.sgmanager.exceptions import *
+from itertools import count
 
 # TODO: diff local and remote groups and their rules
 # TODO: apply differences
@@ -88,19 +89,23 @@ class SecurityGroups(object):
             # For each rule in group
             for rule in group.rules:
                 rule_info = {
-                    'port' : int(rule.to_port),
                     'protocol'  : str(rule.ip_protocol),
-                    'groups' : None,
+                    'groups' : [],
                 }
 
                 if rule.from_port != rule.to_port:
+                    # We have port range, use port_from and port_to parameters
                     rule_info['port_from'] = int(rule.from_port)
+                    rule_info['port_to'] = int(rule.to_port)
+                else:
+                    # We have single port, use port parameter
+                    rule_info['port'] = int(rule.to_port)
 
                 # For each granted permission
                 for grant in rule.grants:
                     try:
                         rule_info['groups'].append({
-                                'name'  : grant.groupName,
+                                'name'  : str(grant.groupName),
                                 'owner' : str(grant.owner_id),
                                 'id'    : str(grant.groupId)
                             })
@@ -162,8 +167,15 @@ class SecurityGroups(object):
         Return YAML dump of loaded groups
         :rtype : basestring
         """
-        # TODO: fixme (broken by refactoring)
-        return yaml.dump(self.groups)
+        return yaml.dump({ name : group.dump() for name, group in self.groups.iteritems() }, Dumper=YamlDumper)
+
+
+class YamlDumper(yaml.SafeDumper):
+    """
+    Custom YAML dumper that will ignore aliases
+    """
+    def ignore_aliases(self, _data):
+        return True
 
 
 class SGroup(object):
@@ -187,40 +199,55 @@ class SGroup(object):
         rule.set_group(self)
         self.rules.append(rule)
 
+    def dump(self):
+        """
+        Return dictionary structure
+        """
+        return {
+            'description' : self.description,
+            'rules'  : [ rule.dump() for rule in self.rules ],
+        }
+
 
 class SRule(object):
     """
     Single security group rule
     """
+    _ids = count(0)
+
     def __init__(self, port=None, port_from=None, port_to=None, groups=[], protocol='tcp', cidr=None):
         """
         Initialize variables
         """
-        # TODO: fix name - we want to identify the rules (index?)
-        self.name = 'TODO'
+        # Set rule id
+        self._ids = self._ids.next()
+        self.name = self._ids
 
         self.protocol = protocol
         self.port = port
         self.port_from = port_from
         self.port_to = port_to
 
-        self.groups = groups
+        self.groups = []
         self.group = None
 
         # Check validity of groups parameter
-        if self.groups:
-            if not isinstance(self.groups, list):
+        if groups:
+            if not isinstance(groups, list):
                 raise InvalidConfiguration('Parameter groups should be list of allowed security groups for rule %s' % self.name)
 
             # Unify format for granted group permissions
             # it has to contain id and group owner (account id)
             for group in groups:
                 if not isinstance(group, dict):
-                    group = {
+                    # Empty owner and id, only name supplied
+                    self.groups.append({
                         'name' : group,
                         'owner': None,
                         'id'   : None,
-                    }
+                    })
+                else:
+                    self.groups.append(group)
 
         # Allow all if we haven't chosen groups or cidr
         if not cidr and not groups:
@@ -244,14 +271,27 @@ class SRule(object):
         """
         Check configuration
         """
-        if not isinstance(self.port, int):
-            raise InvalidConfiguration('Port must be integer for rule %s' % self.name)
+        if self.port and not isinstance(self.port, int):
+            raise InvalidConfiguration('Port must be integer for rule %s not %s' % (self.name, type(self.port).__name__))
 
         if self.port_from and not isinstance(self.port_from, int):
-            raise InvalidConfiguration('Parameter port_from must be integer for rule %s' % self.name)
+            raise InvalidConfiguration('Parameter port_from must be integer for rule %s not %s' % (self.name, type(self.port_from).__name__))
 
         if self.port_to and not isinstance(self.port_to, int):
-            raise InvalidConfiguration('Parameter port_to must be integer for rule %s' % self.name)
+            raise InvalidConfiguration('Parameter port_to must be integer for rule %s not %s' % (self.name, type(self.port_to).__name__))
 
         if self.protocol and self.protocol not in ['tcp', 'udp', 'icmp']:
             raise InvalidConfiguration('Protocol must be tcp, udp or icmp, not %s for rule %s' % (self.protocol, self.name))
+
+    def dump(self):
+        """
+        Return dictionary structure
+        Don't return empty values
+        """
+        result = {}
+
+        for attr in ['protocol', 'port', 'port_to', 'port_from', 'groups']:
+            if getattr(self, attr):
+                result[attr] = getattr(self, attr)
+
+        return result
