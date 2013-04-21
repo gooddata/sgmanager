ff459d827189c4c25e8296649d513947466c70bd
Filip Pytloun <filip.pytloun@gooddata.com>
2013-04-21 22:34:39 +0200

    Log on INFO level by default, add --quiet option

--- gdc/sgmanager/cli.py
+++ gdc/sgmanager/cli.py
@@ -17,13 +17,15 @@ def main():
     parser.add_argument('-c', '--config', help='Config file to use')
     parser.add_argument('--dump', action='store_true', help='Dump remote groups and exit')
     parser.add_argument('-f', '--force', action='store_true', help='Force action (otherwise run dry-run)')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+    parser.add_argument('-q', '--quiet', action='store_true', help='Be quiet, print only WARN/ERROR output')
     parser.add_argument('-d', '--debug', action='store_true', help='Debug mode')
     parser.add_argument('--no-remove', action='store_true', help='Do not remove any groups or rules, only add')
     parser.add_argument('--no-remove-groups', action='store_true', help='Do not remove any groups, only add')
     args = parser.parse_args()
 
-    if args.verbose:
+    if args.quiet:
+        lg.setLevel(logging.WARN)
+    else:
         lg.setLevel(logging.INFO)
 
     if args.debug:
