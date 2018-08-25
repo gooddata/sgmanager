5e2c976bef4e0c2f1d9b5e3a4005df0c4dcb8f14
Igor Gnatenko <igor.gnatenko@gooddata.com>
2018-08-25 19:25:34 +0200

    FEATURE: PC-597 PAAS-8729 Rewrite to use OpenStack SDK
    And make code more readable and better.
    
    Features which did not get implemented during rewrite:
    * Merge groups while dumping
    
    Signed-off-by: Igor Gnatenko <igor.gnatenko@gooddata.com>

--- README.md
+++ README.md
@@ -1,114 +1,30 @@
 # sgmanager
 
-Tooling for management of security groups.
-Load local configuration, load remote groups and apply differences.
+OpenStack Security Groups Management Tool.
 
-## Installation
-With PIP from Github:
+## Configuration
 
-	pip install -e 'git://github.com/gooddata/sgmanager.git#egg=sgmanager'
+See [groups.yaml](examples/groups.yaml) for specification
+and its [expanded version](examples/groups.expanded.yaml).
 
-Or with PIP from Pypi:
+## Installation & Running
 
-	pip install sgmanager
+Running from source tree can be done in 2 ways:
 
-Or from local GIT checkout:
+* Using [flit](https://flit.readthedocs.io): `flit install -s` and then use `sgmanager` from `$PATH`
+* Using Python directly: `python3 -m sgmanager`
 
-	python setup.py install
+Installation can be done using `flit install`. See `--help` from it.
 
-Then you can run it by *sgmanager* command, ensure that you have python bin in your $PATH, eg:
-/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
+## Supplying credentials
 
-Or you can run execution script from checkouted root directory without installation:
+There are [3 standard ways](https://docs.openstack.org/openstacksdk/latest/user/config/configuration.html)
+of passing credentials for OpenStack environments:
 
-	python gdc/sgmanager
+* `--os-*` option for commandline
+* `export OS_*` from environment variables
+* `clouds.yaml` and use `--os-cloud` option
 
-## Usage
-	usage: sgmanager [-h] [-c CONFIG] [--dump] [--unused] [--remove-unused] [-f]
-	                 [-q] [-d] [--no-remove] [--no-remove-groups]
-	                 [-I EC2_ACCESS_KEY] [-S EC2_SECRET_KEY] [-R EC2_REGION]
-	                 [-U EC2_URL] [-t TIMEOUT] [-m MODE] [--insecure]
-	                 [--cert CERT]
+## Notes
 
-	Security groups management tool
-
-	optional arguments:
-	  -h, --help            show this help message and exit
-	  -c CONFIG, --config CONFIG
-	                        Config file to use
-	  --dump                Dump remote groups and exit
-	  --unused              Dump groups not used by any instance
-	  --remove-unused       Only remove groups that are not used by any instance
-	  -f, --force           Force action (otherwise run dry-run)
-	  -q, --quiet           Be quiet, print only WARN/ERROR output
-	  -d, --debug           Debug mode
-	  --no-remove           Do not remove any groups or rules, only add
-	  --no-remove-groups    Do not remove any groups, only add
-	  -I EC2_ACCESS_KEY, --ec2-access-key EC2_ACCESS_KEY
-	                        EC2 Access Key to use
-	  -S EC2_SECRET_KEY, --ec2-secret-key EC2_SECRET_KEY
-	                        EC2 Secret Key to use
-	  -R EC2_REGION, --ec2-region EC2_REGION
-	                        Region to use (default us-east-1)
-	  -U EC2_URL, --ec2-url EC2_URL
-	                        EC2 API URL to use (otherwise use default)
-	  -t TIMEOUT, --timeout TIMEOUT
-	                        Set socket timeout (default 120s)
-	  -m MODE, --mode MODE  Mode for validating group name and description
-	                        (default a)
-	  --insecure            Do not validate SSL certs
-	  --cert CERT           Path to CA certificates (eg. /etc/pki/cacert.pem)
-
-First setup *EC2\_ACCESS\_KEY* and *EC2\_SECRET\_KEY* environment variables. You can also set *EC2\_URL* to connect to custom EC2 endpoint (eg. OpenStack).
-Alternatively set these options from command line.
-
-Setup is submitted by simple YAML configuration, it can be dumped from current EC2 account by running:
-
-	./bin/sgmanager.py --dump > conf/aws-dev.yaml
-
-Then you can edit output yaml file and run following to see the diff:
-
-	./bin/sgmanager.py -c conf/aws-dev.yaml
-
-To apply it, force run with parameter *-f* / *--force*.
-
-To avoid removal of existing groups that aren't present in config file, use parameter *--no-remove*
-
-Each configured group passes validation, group name and description shoul not exceed 255 characters.
-
-There are several modes for string validation of both group name and description, set by *-m* / *--mode*:
-- a, ascii - only ASCII characters
-- s, strict - matches [a-zA-Z0-9_- ]
-- v, vpc - string can contain only a-z, A-Z, 0-9, spaces, and ._-:/()#,@[]+=&;{}!$*
-
-Mode depends on cloud used, for AWS EC2 *-a* is recommended, for AWS EC2 VPC *-v* and for OpenStack with *ec2_strict_validation=on* *-s*
-
-## Configuration options
-	---
-	test1:
-	  description: "SGManager testing security group"
-	  rules:
-		- port: 22
-		  # tcp, udp or icmp
-		  protocol: tcp
-		  # IP ranges that are allowed to connect, can be list or string
-		  cidr: [108.171.171.226/32]
-
-		- port: 80
-		  protocol: tcp
-		  groups:
-			# You can define security group from other account
-			- {name: test2, owner: xyz2137}
-	test2:
-	  description: "SGManager testing security group number 2"
-	  rules:
-		- port_from: 50000
-		  port_to: 50500
-		  groups:
-			- test1
-
-## TODO
-- support for VPC groups
-
-## Development status
-This tool is actively maintained by GoodData.
+- Egress rules have not been tested
---
+++ sgmanager/manager.py
@@ -0,0 +1,246 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright © 2018, GoodData Corporation. All rights reserved.
+
+import logging
+
+from orderedset import OrderedSet
+
+from .group import Group
+from .rule import Rule
+from .utils import validate_groups
+from .yaml import load
+
+logger = logging.getLogger(__name__)
+
+
+class SGManager:
+    '''The Manager.'''
+    def __init__(self, connection=None):
+        self.connection = connection
+        self._local = None
+        self._remote = None
+
+    @property
+    def connection(self):
+        if self._connection is None:
+            raise AttributeError('Connection is not set')
+        return self._connection
+
+    @connection.setter
+    def connection(self, value):
+        self._connection = value
+
+    @property
+    def local(self):
+        if self._local is None:
+            raise AttributeError('Local groups are not loaded')
+        return self._local
+
+    @local.setter
+    def local(self, groups):
+        self._local = OrderedSet(groups)
+
+    @property
+    def remote(self):
+        if self._remote is None:
+            raise AttributeError('Remote groups are not loaded')
+        return self._remote
+
+    @remote.setter
+    def remote(self, groups):
+        self._remote = OrderedSet(groups)
+
+    def load_remote_groups(self):
+        '''Load groups from OpenStack.'''
+        conf = self.connection.list_security_groups()
+
+        self.remote = [Group.from_remote(**info)
+                       for info in conf]
+        return self.remote
+
+    def load_local_groups(self, config):
+        '''Load groups from local configuration file.'''
+        with open(config, 'r') as f:
+            conf = load(f)
+
+        groups = []
+        if not isinstance(conf, dict):
+            raise Exception(
+                f'The topmost collection must be a mapping,'
+                f' not a {type(conf)}')
+
+        def pop_with_type(kwargs, key, typ):
+            prefix = f'Key {key!r}'
+            if key not in kwargs:
+                raise Exception(f'{prefix} must be preset')
+            value = kwargs.pop(key)
+            if not isinstance(value, typ):
+                raise Exception(f'{prefix} must have type {typ},'
+                                f' not a {type(value)}')
+            return value
+
+        document = pop_with_type(conf, 'document', str)
+        if document != 'sgmanager-groups':
+            raise Exception(f'Document type {document!r} must be "sgmanager-groups"')
+        version = pop_with_type(conf, 'version', int)
+        if version != 1:
+            raise Exception(f'Document version {version!r} is not supported')
+        data = pop_with_type(conf, 'data', list)
+
+        if conf:
+            raise Exception(f'Extra keys: {", ".join(conf.keys())}')
+
+        for item in data:
+            name, info = next(iter(item.items()))
+            if len(item.items()) > 1:
+                raise Exception(
+                    f'Syntax error, for item named {name!r}. Missing indent?')
+
+            groups.append(Group.from_local(**{'name': name, **info}))
+
+        self.local = groups
+        return self.local
+
+    def update_remote_groups(self, dry_run=True, threshold=None, remove=True):
+        '''Update remote configuration with the local one.'''
+        # Copy those so that we can modify them even with dry-run
+        local = OrderedSet(self.local)
+        remote = OrderedSet(self.remote)
+
+        validate_groups(local)
+
+        def parse_groups(groups):
+            groups = {group.name: group for group in groups}
+            keys = OrderedSet(groups.keys())
+            return groups, keys
+
+        lgroups, lkeys = parse_groups(local)
+        rgroups, rkeys = parse_groups(remote)
+
+        changes = 0
+        unchanged = 0
+        groups_added = OrderedSet()
+        groups_updated = OrderedSet()
+        groups_removed = OrderedSet()
+        rules_added = OrderedSet()
+        rules_removed = OrderedSet()
+
+        # Added groups
+        for group in (lgroups[name] for name in lkeys - rkeys):
+            grp = Group(group.name, group.description)
+            groups_added.add(grp)
+            rgroups[group.name] = grp
+            rkeys.add(grp.name)
+            changes += 1
+
+        # Changed groups
+        for rgroup, lgroup in ((rgroups[name], lgroups[name])
+                               for name in rkeys & lkeys):
+            if rgroup not in groups_added:
+                unchanged += 1
+
+            if rgroup.description != lgroup.description:
+                # XXX: https://review.openstack.org/596609
+                # groups_updated.add((rgroup, lgroup))
+                pass
+
+            if rgroup.rules != lgroup.rules:
+                # Added rules
+                for rule in lgroup.rules - rgroup.rules:
+                    rules_added.add((rgroup.name, rule))
+                    changes += 1
+
+                # Removed rules
+                for rule in rgroup.rules - lgroup.rules:
+                    if remove:
+                        rules_removed.add((rgroup.name, rule))
+                        changes += 1
+                    else:
+                        unchanged += 1
+            unchanged += len(rgroup.rules & lgroup.rules)
+
+        # Removed groups
+        for group in (rgroups[name] for name in rkeys - lkeys):
+            if remove:
+                groups_removed.add(group)
+                changes += len(group.rules) + 1
+            else:
+                unchanged += len(group.rules) + 1
+
+        if changes == 0 and not groups_updated:
+            return
+
+        # Report result
+        logger.info(f'{changes:d} changes to be made:')
+        for group in groups_added:
+            logger.info(f'  - Create group {group.name!r}')
+        for rgroup, lgroup in groups_updated:
+            logger.info(f'  - Update description for {rgroup.name!r}:'
+                        f' {rgroup.description!r} → {lgroup.description!r}')
+        for group_name, rule in rules_added:
+            logger.info(f'  - Create {rule!r} in group {group_name!r}')
+        for group_name, rule in rules_removed:
+            logger.info(f'  - Remove {rule!r} from group {group_name!r}')
+        for group in groups_removed:
+            logger.info(f'  - Remove group {group.name!r} with {len(group.rules)} rules')
+
+        if threshold is not None:
+            changes_percentage = changes / (unchanged + changes) * 100
+            if changes_percentage > threshold:
+                raise Exception(f'Amount of changes is {changes_percentage:f}%'
+                                f' which is more than allowed ({threshold:f}%)')
+
+        if dry_run:
+            return
+
+        # We've modified 'remote', so copy it again
+        remote = OrderedSet(self.remote)
+        rgroups, rkeys = parse_groups(remote)
+
+        # Added groups
+        for group in groups_added:
+            ginfo = self.connection.create_security_group(
+                name=group.name,
+                description=group.description)
+            remote.add(Group.from_remote(**ginfo))
+        rgroups, rkeys = parse_groups(remote)
+
+        # Updated groups
+        for rgroup, lgroup in groups_updated:
+            self.connection.update_security_group(
+                name_or_id=rgroup._id,
+                description=lgroup.description)
+            # Updating group should not change its ID
+            rgroup.description = lgroup.description
+
+        # Added rules
+        for group_name, rule in rules_added:
+            rgroup = rgroups[group_name]
+            cidr = str(rule.cidr) if rule.cidr is not None else None
+            group_id = rgroups[rule.group]._id if rule.group is not None else None
+            rinfo = self.connection.create_security_group_rule(
+                secgroup_name_or_id=rgroup._id,
+                port_range_min=rule.port_min,
+                port_range_max=rule.port_max,
+                protocol=rule.protocol.value,
+                remote_ip_prefix=cidr,
+                remote_group_id=group_id,
+                direction=rule.direction.value,
+                ethertype=rule.ethertype.value)
+            rgroup.rules.add(Rule.from_remote(**rinfo))
+
+        if remove:
+            # Removed rules
+            for group_name, rule in rules_removed:
+                rgroup = rgroups[group_name]
+                self.connection.delete_security_group_rule(
+                    rule_id=rule._id)
+                rgroup.rules.remove(rule)
+
+            # Removed groups
+            for group in groups_removed:
+                self.connection.delete_security_group(
+                    name_or_id=group._id)
+                remote.remove(group)
+
+        self.remote = remote
